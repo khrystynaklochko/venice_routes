d71bb9012494e791281a3382eec40af5a019c796
khrystynaklochko <khrystynaklochko@gmail.com>
2014-12-04 15:47:17 +0200

    ctoc

--- routes.rb
+++ routes.rb
@@ -6,7 +6,83 @@ module Routes
     end
 
     attr_accessor :routes
+    
+    def from_c_to_c
+      array = @routes
+      array = array.split(", ").sort
+      start_point_index = []
+      end_point_index = []
+      i = 0
+      j = 0
+      array.each do |vertex_pair|
+        if vertex_pair[0] == "C"
+           end_point_index[j] = vertex_pair[1]
+           j = j + 1
+        end
+      end
+      end_point_index.each do |v|
+        if @routes.include?(v+"C")
+          i = i +1
+          else
+            array.each do |vertex_pair|
+              if vertex_pair[0] == v 
+                end_point_index[j] = vertex_pair[1]
+                j = j + 1
+              end
+            end
+          end
+      end
+      end_point_index.each do |v|
+        if @routes.include?(v+"C")
+          i = i + 1
+        end
+      end
+      i
+    end
 
+    def from_a_to_c
+      array = @routes
+      array = array.split(", ").sort
+      start_point_index = []
+      end_point_index = []
+      i = 0
+      j = 0 
+      array.each do |vertex_pair|
+        if vertex_pair[0] == "A"
+           end_point_index[i] = vertex_pair[1]
+           i = i + 1
+        end
+      end
+      end_point_index.each do |v|
+        if @routes.include?(v+"C")
+          else
+            array.each do |vertex_pair|
+              if vertex_pair[0] == v 
+                end_point_index[i] = vertex_pair[1]
+                j = j + 1
+              end
+            end
+          end
+      end
+      end_point_index.each do |v|
+        if @routes.include?(v+"C")
+          else
+            array.each do |vertex_pair|
+              if vertex_pair[0] == v 
+                end_point_index[j] = vertex_pair[1]
+                j = j + 1
+              end
+            end
+          end
+       end
+      i = 0
+      end_point_index.each do |v|
+        if @routes.include?(v+"C")
+          i = i + 1
+        end
+      end
+      i
+    end
 
     def routes_finder
       answers = []
@@ -27,11 +103,14 @@ module Routes
       else answers[3] = 'NO SUCH ROUTE'
       end
       if @routes.include?('AE') && @routes.include?('ED')
-       answers[3] = @routes[@routes.index('AE')+2].to_i + @routes[@routes.index('ED')+2].to_i
-      else answers[3] = 'NO SUCH ROUTE'
+       answers[4] = @routes[@routes.index('AE')+2].to_i + @routes[@routes.index('ED')+2].to_i
+      else answers[4] = 'NO SUCH ROUTE'
       end
+      answers[5] = from_c_to_c
+      answers[6] = from_c_to_c
       answers
     end
+
     def showDistanceMatrix
      # answers = routes_existance
        answers = routes_finder
